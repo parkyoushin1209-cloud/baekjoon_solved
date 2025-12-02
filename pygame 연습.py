9c66c6c48732ffc00ec9ac94a5d8f2c15892d689
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2025-12-03 08:29:03 +0900

    Add files via upload

---
+++ pygame 연습.py
@@ -0,0 +1,16 @@
+t = int(input())
+d = [[0,0] for _ in range(41)]
+d[0] = [1,0]
+d[1] = [0,1]
+array = []
+for _ in range(t):
+    array.append(int(input()))
+m = max(array)
+
+for i in range(2,m+1):
+    d[i][0] = d[i-1][0] + d[i-2][0]
+    d[i][1] = d[i - 1][1] + d[i - 2][1]
+
+for i in array:
+    print(d[i][0], d[i][1])
+
